9108cec01da954d05828821bbe78bf20820d991d
Sandesh Bhusal <59927096+sande5h@users.noreply.github.com>
2025-11-11 22:31:24 +0545

    Add UI manager header file with function declarations

---
+++ ui_manager.h
@@ -0,0 +1,11 @@
+#ifndef UI_MANAGER_H
+#define UI_MANAGER_H
+
+#include "lvgl.h"
+
+void ui_manager_init();
+void ui_manager_show_menu();
+void ui_manager_show_battery();
+void ui_manager_show_weather();
+
+#endif
